86725da96370e8034338c975ddc8aefe7086376e
TarasBandura <bandurataras1596@gmail.com>
2026-03-01 14:58:51 -0500

    partners

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,8 @@ export const metadata: Metadata = {
   description: "Effortless home maintenance with luxury service and honest pricing",
 };
 
+const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID; // ✅ add this env
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html
@@ -31,6 +33,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       </head>
 
+      {/* ✅ GTM */}
       <Script id="gtm" strategy="beforeInteractive">
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -38,6 +41,37 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KFPSD2P6');`}
       </Script>
+
+      {/* ✅ Meta Pixel Base Code */}
+      {FB_PIXEL_ID ? (
+        <>
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${FB_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+
+          <noscript>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+              alt=""
+            />
+          </noscript>
+        </>
+      ) : null}
 
       <body className="antialiased">
         <noscript>
@@ -54,7 +88,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <Providers>
           {children}
 
-          {/* ✅ Special deal popup (homepage-only; component enforces it) */}
           <SpecialDealPopup
             promoCode="SPRING"
             promoEndsLabel="Apr 3"
