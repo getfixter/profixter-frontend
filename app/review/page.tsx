994729109912c2a8f712a7f7a100730794a9803a
TarasBandura <bandurataras1596@gmail.com>
2026-03-27 16:01:37 -0400

    admin improvement

---
+++ app/review/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("https://maps.app.goo.gl/8D8WoA33tKJBZDEG9");
+}
---
+++ app/tip/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("https://buy.stripe.com/eVq8wO3W98O03NL3ASawo00");
+}
